2cf112d99b85824dbdcf2820d43c512d32e0b496
Ivan Mikholap <mikholapivan575@gmail.com>
2026-05-27 18:26:30 +0300

    Initial

---
+++ include/to_upper.h
@@ -0,0 +1,15 @@
+#ifndef TO_UPPER_TO_UPPER_H
+#define TO_UPPER_TO_UPPER_H
+
+#include <cstdlib>
+#include <cstring>
+
+namespace to_upper {
+    constexpr size_t string_size = 256;
+}
+
+constexpr const char* operator""_to_upper(const char* str, size_t len) {
+
+}
+
+#endif //TO_UPPER_TO_UPPER_H
---
+++ tests/to_upper_tests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../include/to_upper.h"
+
+TEST(test, test1) {
+    ASSERT_TRUE(true);
+}
