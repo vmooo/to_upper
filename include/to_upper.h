b5de4d2a1a446239d7b70b3a59bed157251d38b6
Ivan Mikholap <mikholapivan575@gmail.com>
2026-05-30 11:51:29 +0300

    Implement to_upper function that returns the character in uppercase

--- include/to_upper.h
+++ include/to_upper.h
@@ -1,15 +1,27 @@
 #ifndef TO_UPPER_TO_UPPER_H
 #define TO_UPPER_TO_UPPER_H
 
-#include <cstdlib>
-#include <cstring>
+#include <array>
+#include <cassert>
 
-namespace to_upper {
-    constexpr size_t string_size = 256;
+constexpr char to_upper(const char c) {
+   if (c >= 'a' && c <= 'z') {
+      return  static_cast<char>(c - 32);
+   }
+   assert(c >= 0 && c <= 127);
+   return c;
 }
 
-constexpr const char* operator""_to_upper(const char* str, size_t len) {
-
+template<char... chars>
+constexpr auto operator""_upper() {
+   constexpr char str[] = {chars..., '\0'};
+   std::array<char, sizeof...(chars) + 1> result{};
+   for (size_t i = 0; i < sizeof...(chars); ++i) {
+      assert(str[i] != '\0');
+      result[i] = (str[i] >= 'A' && str[i] <= 'Z') ? str[i] : to_upper(str[i]);
+   }
+   result[sizeof...(chars)] = '\0';
+   return result;
 }
 
 #endif //TO_UPPER_TO_UPPER_H
--- tests/to_upper_tests.cpp
+++ tests/to_upper_tests.cpp
@@ -1,6 +1,7 @@
-#include <gtest/gtest.h>
 #include "../include/to_upper.h"
+#include <gtest/gtest.h>
 
-TEST(test, test1) {
-    ASSERT_TRUE(true);
+TEST(test, test_to_upper) {
+    constexpr auto c = to_upper('c');
+    ASSERT_TRUE(c == 'C');
 }
