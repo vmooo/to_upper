89632c60d96a28de4f7afc5728a16a52b3bba268
Ivan Mikholap <mikholapivan575@gmail.com>
2026-05-30 12:35:19 +0300

    Implement _upper literal

--- include/to_upper.h
+++ include/to_upper.h
@@ -3,24 +3,23 @@
 
 #include <array>
 #include <cassert>
+#include <algorithm>
+#include <string_view>
+
+namespace literal_config {
+   constexpr size_t max_size = 256;
+}
 
 constexpr char to_upper(const char c) {
    if (c >= 'a' && c <= 'z') {
       return  static_cast<char>(c - 32);
    }
-   assert(c >= 0 && c <= 127);
    return c;
 }
 
-template<char... chars>
-constexpr auto operator""_upper() {
-   constexpr char str[] = {chars..., '\0'};
-   std::array<char, sizeof...(chars) + 1> result{};
-   for (size_t i = 0; i < sizeof...(chars); ++i) {
-      assert(str[i] != '\0');
-      result[i] = (str[i] >= 'A' && str[i] <= 'Z') ? str[i] : to_upper(str[i]);
-   }
-   result[sizeof...(chars)] = '\0';
+consteval auto operator""_upper(const char* str, const std::size_t len) {
+   std::array<char, literal_config::max_size> result{};
+   std::transform(str, str + len, result.begin(), to_upper);
    return result;
 }
 
--- tests/to_upper_tests.cpp
+++ tests/to_upper_tests.cpp
@@ -1,7 +1,43 @@
 #include "../include/to_upper.h"
 #include <gtest/gtest.h>
+#include <string>
+#include <algorithm>
+#include <array>
 
-TEST(test, test_to_upper) {
+TEST(test, test_to_upper1) {
     constexpr auto c = to_upper('c');
     ASSERT_TRUE(c == 'C');
 }
+
+TEST(test, test_to_upper2) {
+    constexpr auto c = to_upper('z');
+    ASSERT_TRUE(c == 'Z');
+}
+
+TEST(test, test_to_upper3) {
+    constexpr auto c = to_upper('A');
+    ASSERT_TRUE(c == 'A');
+}
+
+TEST(test, test_to_upper4) {
+    constexpr auto c = to_upper('1');
+    ASSERT_TRUE(c == '1');
+}
+
+TEST(test, test_to_upper5) {
+    constexpr auto c = to_upper('\n');
+    ASSERT_TRUE(c == '\n');
+}
+
+TEST(test, test_upper_literal1) {
+    constexpr auto result = "test"_upper;
+    constexpr std::string expected = "TEST";
+    ASSERT_TRUE(std::equal(expected.begin(), expected.end(), result.begin()));
+}
+
+TEST(test, test_upper_literal2) {
+    constexpr auto result = "test2"_upper;
+    constexpr std::string expected = "TEST2";
+    ASSERT_TRUE(std::equal(expected.begin(), expected.end(), result.begin()));
+}
+
